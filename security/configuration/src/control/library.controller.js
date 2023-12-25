d8e6b8f99bfb351a0e4613e62d948f0d7ea072db
seintax <78845146+seintax@users.noreply.github.com>
2023-12-26 01:29:40 +0800

    include data directory

--- security/configuration/src/control/library.controller.js
+++
@@ -1,9 +0,0 @@
-const category = require('../library/category/category.route')
-const discount = require('../library/discount/discount.route')
-const measurement = require('../library/measurement/measurement.route')
-
-module.exports = {
-    category,
-    discount,
-    measurement
-}
--- security/configuration/src/control/system.controller.js
+++
@@ -1,7 +0,0 @@
-const reports = require('../system/reports/reports.route')
-const account = require('../system/account/account.route')
-
-module.exports = {
-    reports,
-    account
-}
